43c09ae33066878687663eb666c9b2caef41caee
root <bogdan1.nicolae@orange.com>
2023-10-24 06:45:08 +0000

    remove upstream parts

--- README.md
+++
@@ -1,8 +0,0 @@
-# Kanod Image Builder
-
-CLI and core library to build custom OS image in Kanod.
-
-Documentation: 
-
-* building and designing images: https://orange-opensource.gitlab.io/kanod/reference/image-builder/index.html
-* core library: https://orange-opensource.gitlab.io/kanod/reference/machines/core/index.html
--- deploy-image.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-REPO_URL=${REPO_URL:-http://192.168.133.100/repository/kanod}
-VERSION=${VERSION:-0.1.0}
-ARTIFACT=${1:-core}
-
-# shellcheck disable=SC2086
-mvn ${MAVEN_CLI_OPTS} --batch-mode -B deploy:deploy-file ${MAVEN_OPTS} -DgroupId=kanod -DartifactId="${ARTIFACT}" -Dversion="${VERSION}" -Dpackaging=qcow2 -Dfile=img.qcow2 -Durl="${REPO_URL}" -DrepositoryId=kanod
-
